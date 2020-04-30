70fa93302b450c94ca40c43ac0490203c87a6b15
SofianD <doual.sofian@hotmail.com>
2020-04-30 18:35:05 +0200

    add NestExpress

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,18 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { NestExpressApplication } from '@nestjs/platform-express';
+// import { json } from 'express';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
+  app.enableCors();
+
+  // If issue 'req too large' exist:
+  // app.use(json({
+  //   limit: '50mb'
+  // }));
+
   await app.listen(3000);
 }
+
 bootstrap();
