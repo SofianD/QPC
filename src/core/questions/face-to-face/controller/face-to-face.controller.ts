5869643859ee0fb495c03bc95160c810ca066a5c
SofianD <doual.sofian@hotmail.com>
2020-06-05 01:09:28 +0200

    add crud function of the face to face controller

--- src/core/questions/face-to-face/controller/face-to-face.controller.ts
+++ src/core/questions/face-to-face/controller/face-to-face.controller.ts
@@ -1,4 +1,43 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Post, UseGuards, Body, Put, Delete } from '@nestjs/common';
+import { FaceToFaceService } from '../service/face-to-face.service';
+import { AuthGuard } from 'src/shared/guards/auth.guard';
+import { FaceToFace } from 'src/shared/models/face-to-face.interface';
 
 @Controller('face-to-face')
-export class FaceToFaceController {}
+export class FaceToFaceController {
+    constructor(
+        private readonly faceService: FaceToFaceService
+    ) {}
+
+    @Get()
+    async getQuestions(): Promise<any> {
+        return;
+    }
+
+    @Get(':id')
+    async getQuestion(): Promise<any> {
+        return;
+    }
+
+    @Post()
+    @UseGuards(AuthGuard)
+    async addQuestion(
+        @Body('questions') questions: FaceToFace[]
+    ) {
+        return;
+    }
+
+    @Put(':id')
+    @UseGuards(AuthGuard)
+    async updateQuestion(
+        @Body('question') question: FaceToFace
+    ) {
+        return;
+    }
+
+    @Delete(':id')
+    @UseGuards(AuthGuard)
+    async deleteQuestion() {
+        return;
+    }
+}
