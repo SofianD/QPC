250dd90a40de8cb3db681b0483ccb6977854f5bc
SofianD <doual.sofian@hotmail.com>
2020-06-05 01:10:03 +0200

    update providers

--- src/core/questions/nine-winning-points/nine-win-points.module.ts
+++ src/core/questions/nine-winning-points/nine-win-points.module.ts
@@ -1,14 +1,19 @@
 import { Module } from '@nestjs/common';
 import { NineWinPointsController } from './controller/nine-win-points.controller';
 import { NineWinPointsService } from './service/nine-win-points.service';
+import { TypegooseModule } from 'nestjs-typegoose';
+import { NineWinPoints } from 'src/shared/models/nine-win-points.model';
+import { QuestionService } from '../question/service/question.service';
 
 @Module({
-    imports: [],
+    imports: [
+        TypegooseModule.forFeature([NineWinPoints])
+    ],
     controllers: [
         NineWinPointsController
     ],
     providers: [
-        NineWinPointsService
+        QuestionService //à modifier si Pierre pas content
     ]
 })
 
