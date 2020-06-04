2eaa61f951c7bb12d5736a8b5a1fb7a55a0d66ce
SofianD <doual.sofian@hotmail.com>
2020-06-05 01:02:55 +0200

    preparation of questions feature

--- src/core/questions/four-in-a-row/controller/four-in-row.controller.ts
+++ src/core/questions/four-in-a-row/controller/four-in-row.controller.ts
@@ -1,4 +1,27 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, HttpException, HttpStatus, Param } from '@nestjs/common';
+import { QuestionService } from '../../question/service/question.service';
 
 @Controller('four-in-row')
-export class FourInRowController {}
+export class FourInRowController {
+
+    constructor(
+        private readonly questionService: QuestionService
+    ) {}
+
+    @Get()
+    async getQuestions() {
+        const questions = await this.questionService.getAll();
+        if (questions.length === 0) throw new HttpException('No question found', HttpStatus.NOT_FOUND);
+
+        return questions;
+    }
+
+    @Get(':id')
+    async getQuestion(
+        @Param('id') id: string
+    ) {
+        const question = await this.questionService.getOne(id);
+        if (question === null) throw new HttpException('Not found' , HttpStatus.NOT_FOUND);
+        return question;
+    }
+}
--- src/core/questions/four-in-a-row/four-in-row.module.ts
+++ src/core/questions/four-in-a-row/four-in-row.module.ts
@@ -1,14 +1,20 @@
 import { Module } from '@nestjs/common';
 import { FourInRowController } from './controller/four-in-row.controller';
 import { FourInRowService } from './service/four-in-row.service';
+import { TypegooseModule } from 'nestjs-typegoose';
+import { QuestionService } from '../question/service/question.service';
+import { FourInRow } from 'src/shared/models/four-in-row.interface';
+
 
 @Module({
-    imports: [],
+    imports: [
+        TypegooseModule.forFeature([FourInRow])
+    ],
     controllers: [
         FourInRowController
     ],
     providers: [
-        FourInRowService
+        QuestionService
     ]
 })
 
