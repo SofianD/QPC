71191bb0138e097f4de4a2cd89a521e4ec82d061
SofianD <doual.sofian@hotmail.com>
2020-06-05 01:11:08 +0200

    add crud functions

--- src/core/questions/nine-winning-points/service/nine-win-points.service.ts
+++ src/core/questions/nine-winning-points/service/nine-win-points.service.ts
@@ -1,4 +1,71 @@
 import { Injectable } from '@nestjs/common';
+import { InjectModel } from 'nestjs-typegoose';
+import { NineWinPoints } from 'src/shared/models/nine-win-points.model';
+import { ReturnModelType } from '@typegoose/typegoose';
 
 @Injectable()
-export class NineWinPointsService {}
+export class NineWinPointsService {
+    constructor(
+        @InjectModel(NineWinPoints) private readonly nwpModel: ReturnModelType<typeof NineWinPoints>
+    ) {}
+
+    async create(
+        question: NineWinPoints
+    ) {
+        const model = new this.nwpModel(question);
+
+        try {
+            const res = await model.save() as any;
+            const {__v, ...createdQuestion} = res._doc;
+            return createdQuestion;
+        } catch (error) {
+            throw Error(error);
+        }
+    }
+
+    async update(
+        id: string,
+        question: NineWinPoints
+    ) {
+        try {
+            const result = await this.nwpModel.updateOne(
+                {_id: id},
+                question
+            );
+            return result.nModified;
+        } catch (error) {
+            throw Error(error);
+        }
+    }
+
+    async delete(
+        id: string
+    ) {
+        try {
+            const result = await this.nwpModel.deleteOne({_id: id});
+            return result;
+        } catch (error) {
+            throw Error(error);
+        }
+    }
+
+    async getOne(
+        id: string
+    ) {
+        try {
+            const result = await this.nwpModel.findById(id) as any;
+            const {__v, ...question} = result._doc;
+            return question;
+        } catch (error) {
+            throw Error(error);
+        }
+    }
+
+    async getAll() {
+        try {
+            return this.nwpModel.find().exec();
+        } catch (error) {
+            throw Error(error);
+        }
+    }
+}
